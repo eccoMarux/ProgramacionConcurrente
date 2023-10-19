91fcdb319bc5894d0da60638ffd09e1cf86b8df4
eccoMarux <mariaelvira.monserratvidal@yahoo.com>
2023-10-19 00:26:29 -0300

    Comentarios extra

--- TP5-SemaforosGenerales/src/Ejercicio03/Comedor.java
+++ TP5-SemaforosGenerales/src/Ejercicio03/Comedor.java
@@ -6,32 +6,32 @@ class Comedor {
     private Semaphore seccionCriticaSemaforo;
     private Semaphore gatos;
     private Semaphore perros;
-    private int cantidadComedores;
+    private int cantComederos;
 
-    public Comedor(int cantidad) {
-        this.cantidadComedores = cantidad;
+    public Comedor(int cantidadComederos) {
+        this.cantComederos = cantidadComederos;
         this.seccionCriticaSemaforo = new Semaphore(1);
-        this.gatos = new Semaphore(cantidad);
-        this.perros = new Semaphore(cantidad);
+        this.gatos = new Semaphore(cantidadComederos);
+        this.perros = new Semaphore(cantidadComederos);
     }
 
-    /*
-     * PENSAR!
-     * Hola! COMENTARIO!! +
-     * CUIDADO! Creo que se puede obviar la variable cantidad de comederos +
-     * y evaluar en base a semaforos de perros y gatos que ya manejan la cantidad. +
+    /* PENSAR!
+     * Hola! Dejo COMENTARIOS Y MEJORAS!! 
+     * CUIDADO 1! Creo que se puede obviar la variable cantComederos de comederos 
+     * y evaluar en base a semaforos de perros y gatos que ya manejan la cantComederos. 
+     * CUIDADO 2! Verificar si liberarComedor() puede trabajar de otra forma.
      */
 
     public void comeGato() throws InterruptedException {
-        seccionCriticaSemaforo.acquire();
-        if (cantidadComedores > 0 && gatos.availablePermits() > 0) {
+        seccionCriticaSemaforo.acquire();//INICIO SECCION CRITICA.
+        if (cantComederos > 0 && gatos.availablePermits() > 0) {
             gatos.acquire(); // Adquiere un plato.
-            cantidadComedores--;
+            cantComederos--;
             // Informacion:
             System.out.println("Gatito está comiendo :3");
-            seccionCriticaSemaforo.release(); // Una vez aqui puede soltar el permiso.
+            seccionCriticaSemaforo.release(); //FIN SECCION CRITICA.
             Thread.sleep(1000); // Simulación de que come.
-            gatos.release(); // Libera el plato
+            gatos.release(); // Libera el plato.
             // Informacion:
             System.out.println("Gatito terminó de comer :3");
         } else {
@@ -41,14 +41,14 @@ class Comedor {
     }
 
     public void comePerro() throws InterruptedException {
-        seccionCriticaSemaforo.acquire();
-        if (cantidadComedores > 0 && perros.availablePermits() > 0) {
+        seccionCriticaSemaforo.acquire();//INICIO SECCION CRITICA.
+        if (cantComederos > 0 && perros.availablePermits() > 0) {
             perros.acquire();// Adquiere un plato.
-            cantidadComedores--;
+            cantComederos--;
             System.out.println("Perro comiendo :P");
-            seccionCriticaSemaforo.release(); // Una vez aqui puede soltar el permiso.
+            seccionCriticaSemaforo.release(); // FIN SECCION CRITICA.
             Thread.sleep(1000); // Simulación de que come.
-            perros.release();
+            perros.release(); // Libera el plato.
             // Informacion:
             System.out.println("Perro terminó de comer :P");
         } else {
@@ -61,14 +61,14 @@ class Comedor {
     public void liberarComedor() {
         boolean adquirido = false;
         try {
-            adquirido = seccionCriticaSemaforo.tryAcquire(); // Intentar adquirir el semáforo de la sección crítica
+            adquirido = seccionCriticaSemaforo.tryAcquire(); // Intentar adquirir el semáforo de la sección crítica.
             if (adquirido) {
-                cantidadComedores++;
+                cantComederos++;
             } else {
                 System.out.println("¡¡¡No se pudo adquirir el semáforo en liberarComedor()!!!");
             }
-        } finally {
-            if (adquirido) { // Liberar el semáforo si fue adquirido con éxito
+        } finally { 
+            if (adquirido) { // Liberar el semáforo si fue adquirido.
                 seccionCriticaSemaforo.release();
             }
         }
